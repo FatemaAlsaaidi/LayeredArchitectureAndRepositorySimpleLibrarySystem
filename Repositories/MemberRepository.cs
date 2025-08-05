3b36174980ecb1ca6ea4f2d42714d05f6b8c3d27
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 09:16:37 +0400

    Methos for add new member

--- Repositories/MemberRepository.cs
+++ Repositories/MemberRepository.cs
@@ -9,10 +9,21 @@ namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Repositories
     class MemberRepository
     {
         // method for return all members from the data source
-        public List<Models.Book> GetAllMember()
+        public List<Models.Member> GetAllMember()
         {
-            return FileContext.LoadDataFromFile<Models.Book>(FileContext.FileMembersPath);
+            return FileContext.LoadDataFromFile<Models.Member>(FileContext.FileMembersPath);
 
+        }
+
+        // Methos for add new member 
+        public void AddMember(Models.Member member)
+        {
+            // Load existing members
+            var members = GetAllMember();
+            // Add the new member to the list
+            members.Add(member);
+            // Save the updated list back to the file
+            FileContext.SaveDataToFile(members, FileContext.FileMembersPath);
         }
     }
 }
