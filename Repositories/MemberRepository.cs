f37e0083d3fe4b3fd14fd7ebbd9309e6f0a3f5d9
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 09:12:25 +0400

    method for return all members from the data source

---
+++ Repositories/MemberRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Repositories
+{
+    class MemberRepository
+    {
+        // method for return all members from the data source
+        public List<Models.Book> GetAllMember()
+        {
+            return FileContext.LoadDataFromFile<Models.Book>(FileContext.FileMembersPath);
+
+        }
+    }
+}
