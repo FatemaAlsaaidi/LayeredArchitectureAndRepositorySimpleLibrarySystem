f6615e7bbc013d743c0a611567f7a085ea7a7182
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 08:25:23 +0400

    Represents a record of a book borrowed by a member

---
+++ Models/BorrowRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Models
+{
+    class BorrowRecord
+    {
+        // Represents a record of a book borrowed by a member
+        public static int RecordCount { get; set; } = 0; // Static property to keep track of the number of borrow records
+        public int RecordId { get; set; }
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowDate { get; set; }
+    }
+}
