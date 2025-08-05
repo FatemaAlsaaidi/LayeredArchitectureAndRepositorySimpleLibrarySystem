21b9866f5f87637bf028b7c46ff40de7dabc7b4c
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 09:16:23 +0400

    method to return all borrow records from the data source

---
+++ Repositories/BorrowRecordRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Repositories
+{
+    class BorrowRecordRepository
+    {
+        //method to return all borrow records from the data source
+        public List<Models.BorrowRecord> GetAllBorrowRecords()
+        {
+            return FileContext.LoadDataFromFile<Models.BorrowRecord>(FileContext.FileBorrowRecordPath);
+        }
+    }
+}
