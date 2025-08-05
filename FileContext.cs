85c65f6107607d2e0f45420e212040b0da0f81ea
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 08:26:32 +0400

    File paths for storing data

---
+++ FileContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem
+{
+    class FileContext
+    {
+        // File paths for storing data
+        public static string FileBookPath = "Books.json";
+        public static string FileMembersPath = "Members.json";
+        public static string FileBorrowRecordPath = "BorrowRecord.json";
+
+       
+
+    }
+
+}
