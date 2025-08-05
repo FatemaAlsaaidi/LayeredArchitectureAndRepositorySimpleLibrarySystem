896e849a577f7dc4a48d710ae56cce374b9a08d2
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 08:24:45 +0400

    Properties a member in the library system

---
+++ Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Models
+{
+    class Member
+    {
+        // Properties a member in the library system
+        public static int MemberCount { get; set; } = 0;
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+    }
+}
