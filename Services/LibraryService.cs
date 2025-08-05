8bff321fd9a0068927bb821973950186e883b287
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 14:57:22 +0400

    update

---
+++ Services/LibraryService.cs
@@ -0,0 +1,27 @@
+﻿using LayeredArchitectureAndRepositorySimpleLibrarySystem.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Services
+{
+    class LibraryService
+    {
+        // 
+        public void AddBook(string title, string author, bool isAvailable)
+        {
+            // Create a new book instance
+            var newBook = new Models.Book
+            {
+                BookId = ++Models.Book.BookCount,
+                BookTitle = title,
+                BookAuthor = author,
+                IsAvailable = isAvailable
+            };
+            // bookRepo.AddBook(newBook);
+            Console.WriteLine($"Book '{newBook.BookTitle}' by {newBook.BookAuthor} added successfully.");
+        }
+    }
+}
