b600284d21fb3050959262834a3575cda25d0346
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 08:23:10 +0400

    properties a book in the library system

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Models
+{
+    class Book
+    {
+        // properties a book in the library system
+        public static int BookCount { get; set; } = 0;
+        public int BookId { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
