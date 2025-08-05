df03f94b95785ca0d6ba2543da3e67dc90a33ffb
Fatema Alsaaidi <116080014+FatemaAlsaaidi@users.noreply.github.com>
2025-08-05 08:55:43 +0400

    Method to load All books from a file

---
+++ Repositories/BookRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayeredArchitectureAndRepositorySimpleLibrarySystem.Repositories
+{
+    class BookRepository
+    {
+        //Method to load All books from a file
+        public List<Models.Book> GetAllBooks()
+        {
+            // This method should return all books from the data source
+            return FileContext.LoadDataFromFile<Models.Book>(FileContext.FileBookPath);
+        }
+       
+
+
+    }
+}
